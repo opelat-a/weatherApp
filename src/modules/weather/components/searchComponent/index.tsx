d8f30ca52f442cbd58419256d28358eb6eb5630b
Aleksey <opelat.a@gmail.com>
2021-02-14 23:06:23 +0200

    fix h1 / delete async store / fix width

--- src/modules/weather/components/searchComponent/index.tsx
+++ src/modules/weather/components/searchComponent/index.tsx
@@ -3,7 +3,7 @@ import isEqual from 'react-fast-compare';
 import {useDispatch} from 'react-redux';
 //
 import {setWeatherError} from 'modules/weather/redux/actions';
-import {Button, Text, TextInput} from 'modules/weather/styles';
+import {Button, MainContainer, Text, TextInput} from 'modules/weather/styles';
 import {useWeather} from 'modules/weather/useWeather';
 import {Localization} from 'services/localization';
 import {useSelectorTs} from 'services/selector';
@@ -26,16 +26,23 @@ export const WeatherSerchComponent = memo(() => {
   };
 
   return (
-    <>
+    <MainContainer>
+      <Text margin strong size="18px">
+        Узнай погоду в любом городе!
+      </Text>
       {isError && (
-        <Text color="red">
+        <Text color="#ff4d4d">
           {Localization.responseStatus['' + isError] || Localization.error}
         </Text>
       )}
-      <TextInput onChangeText={(text) => onChangeText(text)} value={city} />
+      <TextInput
+        onChangeText={(text) => onChangeText(text)}
+        value={city}
+        placeholder="Введите свой город"
+      />
       <Button onPress={() => onPressHandler(city)} disabled={!city}>
         <Text>{Localization.searchWeather}</Text>
       </Button>
-    </>
+    </MainContainer>
   );
 });
--- src/modules/weather/useWeather.ts
+++ src/modules/weather/useWeather.ts
@@ -2,6 +2,7 @@ import React from 'react';
 import {useDispatch} from 'react-redux';
 import {getWeatherWithName} from './saga/actions';
 import {setWeatherWithName} from './redux/actions';
+import {deleteAsyncStore, AsyncStoreKeys} from 'services/asyncStore';
 
 export const useWeather = () => {
   const dispatch = useDispatch();
@@ -12,6 +13,7 @@ export const useWeather = () => {
 
   const anotherCity = () => {
     dispatch(setWeatherWithName(null));
+    deleteAsyncStore(AsyncStoreKeys.weather);
   };
 
   return {getWeather, anotherCity};
